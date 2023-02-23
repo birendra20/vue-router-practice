55701c5206fa3cb1a6511807b51d0f25a84dc8d9
birendra gupta <birendragupta20@gmail.com>
2023-02-23 17:46:46 +0530

    routes done

---
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="Jobs">
+    <h1>Job Page</h1>
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Jobs",
+  components: {},
+  data() {
+    return {
+      jobs: [
+        { title: "react developer", id: 1, details: "random text react" },
+        { title: "vue developer", id: 1, details: "random text vue" },
+        { title: "angular developer", id: 1, details: "random text angular" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.job h2 {
+  background: gainsboro;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: rgba(65, 55, 54, 0.777);
+}
+.job h2:hover {
+  background: whitesmoke;
+}
+
+.job a {
+  text-decoration: none;
+}
+</style>
---
+++ ninja-jobs/src/views/jobs/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>404</h2>
+    <h3>page Not found</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style></style>
