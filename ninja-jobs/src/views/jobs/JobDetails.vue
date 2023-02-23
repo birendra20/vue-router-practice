e17ee1e3db63828c6ad166e9b7aaf058ff29f945
birendra gupta <birendragupta20@gmail.com>
2023-02-23 17:46:36 +0530

    routing done

---
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Jobs Details</h1>
+    <p>The Job id is {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"], //from router/index.js
+  name: "JobDetails",
+  //   data() {
+  //     return {
+  //       id: this.$route.params.id,
+  //     };
+  //   },
+};
+</script>
+
+<style></style>
